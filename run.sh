39d068c3701a49b36855028524da6bb1ca22a9f6
karthik-t-g <karthikeyangopal16@gmail.com>
2025-02-19 16:05:54 +0100

    run

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 git pull;
-ansible-playbook -i $1.dev.karthi.com, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$1 main.yml
+ansible-playbook -i $1.dev.karthi.com, -e env=dev -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$1 main.yml
